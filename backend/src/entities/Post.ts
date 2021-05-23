4ea495b8715b12b929d9c058251c85618cbf32bc
Ng Jun Kang <ng_jun_kang@hotmail.com>
2021-05-23 20:37:44 +0800

    Added temporary post entity

---
+++ backend/src/entities/Post.ts
@@ -0,0 +1,35 @@
+import { Field, ObjectType } from "type-graphql";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class Post extends BaseEntity {
+  // extending BaseEntity allows User.create..
+
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  content!: string;
+}
