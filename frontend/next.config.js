3d0d6a23453277dcbbfc3205b92da057ac79d4f1
Ng Jun Kang <ng_jun_kang@hotmail.com>
2021-05-23 15:46:07 +0800

    Added headers to test HSTS

---
+++ frontend/next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+        ],
+      },
+    ]
+  },
+}
