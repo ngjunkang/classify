274462b9948f8426d5d36c32ec5982fb8607752d
pangyuanker <e0660595@u.nus.edu>
2021-06-16 03:03:37 -0600

    updated post resolver for pagination

--- backend/src/resolvers/post.ts
+++ backend/src/resolvers/post.ts
@@ -1,7 +1,21 @@
 import { Post } from "../entities/Post";
-import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  FieldResolver,
+  InputType,
+  Int,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+  Root,
+  UseMiddleware,
+} from "type-graphql";
 import { ThisContext } from "src/types";
 import isAuth from "../middlewares/isAuth";
+import { getConnection } from "typeorm";
 
 @InputType()
 class CreatePostDetails {
@@ -12,11 +26,43 @@ class CreatePostDetails {
   content: string;
 }
 
-@Resolver()
+@ObjectType()
+class PaginatedPosts {
+  @Field(() => [Post])
+  posts: Post[];
+  @Field()
+  hasMore: boolean;
+}
+
+@Resolver(Post)
 export class PostResolver {
-  @Query(() => [Post])
-  async posts(): Promise<Post[]> {
-    return Post.find({});
+  @FieldResolver(() => String)
+  textSnippet(@Root() root: Post) {
+    return root.content.slice(0, 50);
+  }
+
+  @Query(() => PaginatedPosts)
+  async posts(
+    @Arg("limit", () => Int) limit: number,
+    @Arg("cursor", () => String, { nullable: true }) cursor: string | null
+  ): Promise<PaginatedPosts> {
+    const realLimit = Math.min(50, limit);
+    const realLimitPlusOne = realLimit + 1;
+    const qb = getConnection()
+      .getRepository(Post)
+      .createQueryBuilder("p")
+      .orderBy('"createdAt"', "DESC")
+      .take(realLimitPlusOne);
+    if (cursor) {
+      qb.where('"createdAt" < :cursor', {
+        cursor: new Date(parseInt(cursor)),
+      });
+    }
+    const posts = await qb.getMany();
+    return {
+      posts: posts.slice(0, realLimit),
+      hasMore: posts.length === realLimitPlusOne,
+    };
   }
 
   @Query(() => Post, { nullable: true })
@@ -36,23 +82,24 @@ export class PostResolver {
     }).save();
   }
 
-  @Mutation(() => Post, {nullable: true })
+  @Mutation(() => Post, { nullable: true })
   async updatePost(
-      @Arg('id') id: number,
-      @Arg('title', () => String, { nullable: true }) title: string): Promise<Post | null> {
-          const post = await Post.findOne(id);
-          if (!post) {
-              return null
-          }
-          if (typeof title !== 'undefined') {
-              Post.update({id}, {title});
-          }
-      return post;
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string
+  ): Promise<Post | null> {
+    const post = await Post.findOne(id);
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      Post.update({ id }, { title });
+    }
+    return post;
   }
 
   @Mutation(() => Boolean)
-  async deletePost(@Arg('id') id: number): Promise<boolean> {
-          await Post.delete(id);
-          return true;
+  async deletePost(@Arg("id") id: number): Promise<boolean> {
+    await Post.delete(id);
+    return true;
   }
 }
