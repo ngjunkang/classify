20a6d32734877dd8b38071a29fd9b544a2c57109
Ng Jun Kang <ng_jun_kang@hotmail.com>
2021-06-18 21:10:25 +0800

    Added Group, GroupInvite, GroupMessage, GroupRequest, GroupUser, Membership and Module entities into Typeorm connection

--- backend/src/typeormconfig.ts
+++ backend/src/typeormconfig.ts
@@ -2,11 +2,28 @@ import { User } from "./entities/User";
 import { createConnection } from "typeorm";
 import { Post } from "./entities/Post";
 import "dotenv-safe/config";
+import { Module } from "./entities/Module";
+import { Group } from "./entities/Group";
+import { GroupInvite } from "./entities/GroupInvite";
+import { GroupMessage } from "./entities/GroupMessage";
+import { GroupRequest } from "./entities/GroupRequest";
+import { GroupUser } from "./entities/GroupUser";
+import { Membership } from "./entities/Membership";
 
 export default {
   type: "postgres",
   url: process.env.DATABASE_URL,
   logging: true,
   synchronize: true,
-  entities: [User, Post],
+  entities: [
+    User,
+    Post,
+    Group,
+    GroupInvite,
+    GroupRequest,
+    GroupMessage,
+    GroupUser,
+    Membership,
+    Module,
+  ],
 } as Parameters<typeof createConnection>[0];
