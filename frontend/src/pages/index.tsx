1c9368b2e7994aa0e9f4eb1113a0e7ca8a081bb1
pangyuanker <e0660595@u.nus.edu>
2021-06-18 00:23:42 -0600

    Added temporary fix and functionality to index page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,6 +3,8 @@ import Layout from "../components/Layout";
 import CreateUrqlClient from "../utils/CreateUrqlClient";
 import { useMeQuery, usePostsQuery } from "../generated/graphql";
 import isServer from "../utils/isServer";
+import NextLink from "next/link";
+import React from "react";
 
 const Index = () => {
   const [{ data, fetching }] = usePostsQuery();
@@ -10,20 +12,19 @@ const Index = () => {
     pause: isServer(),
   });
 
-  let posts = null;
-  if (data) {
-    posts = data.posts.map(({ title, content, id }) => {
-      return <div key={id}>{title + ": " + content}</div>;
-    });
-  }
-
   return (
     <Layout variant="regular">
       <div>{`Welcome${
         me?.me ? ", " + me.me.username : ""
       }! Please follow for more updates! Feel free to test the login/register/reset password/forgot password functions`}</div>
       <br />
-      {fetching ? "loading" : posts}
+      <NextLink href="/create-post">
+        <a> create post</a>
+      </NextLink>
+      <br />
+      <NextLink href="/forum">
+        <a> forum</a>
+      </NextLink>
     </Layout>
   );
 };
