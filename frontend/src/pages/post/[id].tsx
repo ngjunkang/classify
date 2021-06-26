2756cdb757bde9dbdc48b81c4abe4278b57f9493
pangyuanker <e0660595@u.nus.edu>
2021-06-26 01:59:30 -0600

    Added styling for individual post page

--- frontend/src/pages/post/[id].tsx
+++ frontend/src/pages/post/[id].tsx
@@ -1,4 +1,5 @@
-import { Box, Theme } from "@material-ui/core";
+import { Box, Button, Paper, Theme, Typography } from "@material-ui/core";
+import { lightBlue } from "@material-ui/core/colors";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
@@ -16,24 +17,23 @@ const useStyles = makeStyles((theme: Theme) =>
       display: "",
       flexDirection: "column",
       alignItems: "center",
-    },
-    flexBoxRight: {
-      margin: 2,
-      justifyContent: "flex-end",
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "right",
+      padding: 15,
     },
     flexBox: {
       display: "flex",
     },
     flexGrowContent: {
       flexGrow: 1,
+      padding: 15,
+    },
+    button: {
+      color: lightBlue[500],
     },
   })
 );
 
 const Post = ({}) => {
+  const router = useRouter();
   const styles = useStyles();
   const [{ data, fetching }] = postFromUrl();
   if (fetching) {
@@ -51,13 +51,28 @@ const Post = ({}) => {
     );
   }
   return (
-    <Layout>
-      <text>
-        Page is still under construction, but info from post is visible below:
-      </text>
-      <br />
-      <h1>title: {data?.post?.title}</h1>
-      <text>content: {data?.post?.content}</text>
+    <Layout variant="regular">
+      <Paper key={data?.post?.id} className={styles.flexBoxCol} elevation={2}>
+        <Box className={styles.flexGrowContent}>
+          <Typography variant="h3" color="primary">
+            {data?.post?.title}
+          </Typography>
+          <text>
+            {data?.post?.creator ? `by ${data?.post?.creator.username}` : null}
+          </text>
+        </Box>
+        <Box className={styles.flexGrowContent}>
+          <Typography variant="body1">{data?.post?.content}</Typography>
+        </Box>
+        <Box>
+          <Button
+            className={styles.button}
+            onClick={() => router.push("/whiteboard")}
+          >
+            Back to Whiteboard
+          </Button>
+        </Box>
+      </Paper>
     </Layout>
   );
 };
