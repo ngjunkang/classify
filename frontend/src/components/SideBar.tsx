8d948ef563b9f59b8055798aeb53c31224f0c465
Ng Jun Kang <ng_jun_kang@hotmail.com>
2021-06-27 20:08:02 +0800

    Added sidebar component

---
+++ frontend/src/components/SideBar.tsx
@@ -0,0 +1,55 @@
+import {
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import NextLink from "next/link";
+
+interface SideBarProps {
+  open: boolean;
+  toggleDrawer: (
+    open: boolean
+  ) => (event: React.KeyboardEvent | React.MouseEvent) => void;
+}
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+});
+
+const routes = [
+  { label: "Whiteboard", route: "/whiteboard" },
+  { label: "Cliques", route: "/class/cliques" },
+];
+
+const SideBar: React.FC<SideBarProps> = ({ open, toggleDrawer }) => {
+  const classes = useStyles();
+
+  return (
+    <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
+      <div
+        className={classes.list}
+        role="presentation"
+        onClick={toggleDrawer(false)}
+        onKeyDown={toggleDrawer(false)}
+      >
+        <List>
+          {routes.map(({ label, route }, index) => (
+            <NextLink href={route} key={index}>
+              <ListItem button>
+                <ListItemText primary={label} />
+              </ListItem>
+            </NextLink>
+          ))}
+        </List>
+      </div>
+    </Drawer>
+  );
+};
+
+export default SideBar;
