39d77c8c82a84e8de3015142a7a509073528a801
Ng Jun Kang <ng_jun_kang@hotmail.com>
2021-06-27 20:45:46 +0800

    Edited index page by yk

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,31 +1,95 @@
-import { Link, Typography } from "@material-ui/core";
+import { Box, Button, Theme, Typography } from "@material-ui/core";
+import { lightBlue } from "@material-ui/core/colors";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 import { withUrqlClient } from "next-urql";
-import NextLink from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import Layout from "../components/Layout";
 import { useMeQuery } from "../generated/graphql";
 import CreateUrqlClient from "../utils/CreateUrqlClient";
 import isServer from "../utils/isServer";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    flexBoxCol: {
+      margin: 2,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      padding: 15,
+    },
+    flexBoxRow: {
+      margin: 2,
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+    },
+    buttonLeft: {
+      color: "white",
+      backgroundColor: lightBlue[500],
+      marginTop: 10,
+    },
+    buttonRight: {
+      color: "white",
+      backgroundColor: lightBlue[500],
+      marginRight: "auto",
+      marginTop: 10,
+      marginLeft: 8,
+    },
+  })
+);
+
 const Index = () => {
+  const router = useRouter();
+  const styles = useStyles();
   const [{ data: me }] = useMeQuery({
     pause: isServer(),
   });
 
   return (
     <Layout variant="regular">
-      <Typography>{`Welcome${
-        me?.me ? ", " + me.me.username : ""
-      }! Please follow for more updates! Feel free to test the whiteboard and the cliques features!`}</Typography>
+      {me?.me ? (
+        <Typography>{`Hello${
+          me?.me ? ", " + me.me.username : ""
+        }!`}</Typography>
+      ) : null}
       <br />
-
-      <NextLink href="/whiteboard">
-        <Link>Whiteboard</Link>
-      </NextLink>
-      <br />
-      <NextLink href="/class/cliques">
-        <Link>Cliques</Link>
-      </NextLink>
+      <Box className={styles.flexBoxCol}>
+        <Typography variant="h2" color="primary">
+          Welcome to Classify!
+        </Typography>
+        {me?.me ? (
+          <Box className={styles.flexBoxRow}>
+            <Button
+              className={styles.buttonLeft}
+              onClick={() => router.push("/whiteboard")}
+            >
+              To Whiteboard
+            </Button>
+            <Button
+              className={styles.buttonRight}
+              onClick={() => router.push("/class/cliques")}
+            >
+              To Cliques
+            </Button>
+          </Box>
+        ) : (
+          <Box className={styles.flexBoxRow}>
+            <Button
+              className={styles.buttonLeft}
+              onClick={() => router.push("/login")}
+            >
+              Login
+            </Button>
+            <Button
+              className={styles.buttonRight}
+              onClick={() => router.push("/register")}
+            >
+              register
+            </Button>
+          </Box>
+        )}
+      </Box>
     </Layout>
   );
 };
