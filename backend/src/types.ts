4a4970ec09a4168c1ac5705ecb16bc759cd70d7c
Ng Jun Kang <ng_jun_kang@hotmail.com>
2021-07-17 20:02:05 +0800

    Added user dataloader and SubscriptionContext type for subscription

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { Session, SessionData } from "express-session";
 import { Redis } from "ioredis";
 import createModuleLoader from "./utils/createModuleLoader";
+import createUserLoader from "./utils/createUserLoader";
 
 export type ThisContext = {
   req: Request & {
@@ -10,4 +11,11 @@ export type ThisContext = {
   res: Response;
   redis: Redis;
   moduleLoader: ReturnType<typeof createModuleLoader>;
+  userLoader: ReturnType<typeof createUserLoader>;
+};
+
+export type SubscriptionContext = {
+  subcriptionRequest: Request & {
+    session: Session & Partial<SessionData> & { userId: number };
+  };
 };
