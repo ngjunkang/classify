55cb4c622202fbda4f65419e3c64dc2797e18aec
Ng Jun Kang <ng_jun_kang@hotmail.com>
2021-07-17 19:59:40 +0800

    added NEW_GROUP_MESSAGE_TOPIC constant for subscription

--- backend/src/constant.ts
+++ backend/src/constant.ts
@@ -1,3 +1,4 @@
 export const PRODUCTION = process.env.NODE_ENV === "production";
 export const COOKIE_NAME = "cid";
 export const RESET_PASSWORD_PREFIX = "reset-password:";
+export const NEW_GROUP_MESSAGE_TOPIC = "NEW_GROUP_MESSAGE";
